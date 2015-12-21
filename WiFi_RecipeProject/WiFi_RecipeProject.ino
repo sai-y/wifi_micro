9012b34a1683d9f4890b64c984b2b964083854e6
sai-y <sai@altschool.com>
2015-12-20 20:37:36 -0800

    Checking in v2

--- WiFi_RecipeProject/WiFi_RecipeProject.ino
+++ WiFi_RecipeProject/WiFi_RecipeProject.ino
@@ -1,162 +1,110 @@
-/*
-
- Udp NTP Client
-
- Get the time from a Network Time Protocol (NTP) time server
- Demonstrates use of UDP sendPacket and ReceivePacket
- For more on NTP time servers and the messages needed to communicate with them,
- see http://en.wikipedia.org/wiki/Network_Time_Protocol
-
- created 4 Sep 2010
- by Michael Margolis
- modified 9 Apr 2012
- by Tom Igoe
- 
- Modified 1 July 2014
- by Noah Luskey
-
- This code is in the public domain.
-
- */
-
-#ifndef __CC3200R1M1RGC__
-// Do not include SPI for CC3200 LaunchPad
-#include <SPI.h>
-#endif
 #include <WiFi.h>
+#include <WiFiClient.h>
 
-// your network name also called SSID
-char ssid[] = "Hogwarts";
-// your network password
-char password[] = "Expectopatronum1987";
-char server[] = "dot.ca.gov";
+#include <Temboo.h>
+#include "TembooAccount.h" // Contains Temboo account information
 
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-
-IPAddress timeServer(206,246,122,250); // time.nist.gov NTP server
-
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP Udp;
 WiFiClient client;
-void setup()
-{
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  while(!Serial);
-  // attempt to connect to Wifi network:
-  Serial.print("Attempting to connect to Network named: ");
-  // print the network name (SSID);
-  Serial.println(ssid); 
-  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-  WiFi.begin(ssid, password);
-  while ( WiFi.status() != WL_CONNECTED) {
-    // print dots while we wait to connect
-    Serial.print(".");
-    delay(300);
-  }
+
+void setup() {
   
-  Serial.println("\nYou're connected to the network");
-  Serial.println("Waiting for an ip address");
-  
-  while (WiFi.localIP() == INADDR_NONE) {
-    // print dots while we wait for an ip addresss
-    Serial.print(".");
-    delay(300);
+  Serial.begin(9600);
+  pinMode(7, INPUT);
+  int wifiStatus = WL_IDLE_STATUS;
+
+  // Determine if the WiFi Shield is present
+  Serial.print("\n\nShield:");
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("FAIL");
+
+    // If there's no WiFi shield, stop here
+    while(true);
   }
 
-  Serial.println("\nIP Address obtained");
-  
-  // you're connected now, so print out the status  
-  printWifiStatus();
+  Serial.println("OK");
 
-  if(client.connect(server, 80)){
-    Serial.println("Connected to server");
-    client.println("GET /hq/roadinfo/sr108 HTTP/1.1");
-    client.println("Host: energia.nu");
-    client.println("Connection: close");
-    client.println();
+  // Try to connect to the local WiFi network
+  while(wifiStatus != WL_CONNECTED) {
+    Serial.print("WiFi:");
+    wifiStatus = WiFi.begin(WIFI_SSID, WPA_PASSWORD);
+
+    if (wifiStatus == WL_CONNECTED) {
+      Serial.println("OK");
+    } else {
+      Serial.println("FAIL");
+    }
+    delay(5000);
+  }
+
+  Serial.println("Setup complete.\n");
+}
+
+void loop() {
+  if (!digitalRead(7)) {
+
+    TembooChoreo GetSearchResultChoreo(client);
+    TembooChoreo StatusesUpdateChoreo(client);
     
+    // Invoke the Temboo client
+    GetSearchResultChoreo.begin();
+    // Invoke the Temboo client
+    StatusesUpdateChoreo.begin();
+
+    // Set Temboo account credentials
+    GetSearchResultChoreo.setAccountName(TEMBOO_ACCOUNT);
+    GetSearchResultChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+    GetSearchResultChoreo.setAppKey(TEMBOO_APP_KEY);
+    
+    // Set Temboo account credentials
+    StatusesUpdateChoreo.setAccountName(TEMBOO_ACCOUNT);
+    StatusesUpdateChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+    StatusesUpdateChoreo.setAppKey(TEMBOO_APP_KEY);
+
+    // Set profile to use for execution
+    GetSearchResultChoreo.setProfile("WolframAlphaAccount");
+    // Set profile to use for execution
+    StatusesUpdateChoreo.setProfile("twitternew");
+
+    // Set Choreo inputs
+    String InputValue = "tell me a  joke";
+    GetSearchResultChoreo.addInput("Input", InputValue);
+
+    // Identify the Choreo to run
+    GetSearchResultChoreo.setChoreo("/Library/WolframAlpha/GetSearchResult");
+    // Identify the Choreo to run
+    StatusesUpdateChoreo.setChoreo("/Library/Twitter/Tweets/StatusesUpdate");
+    
+    // Run the Choreo; when results are available, print them to serial
+    GetSearchResultChoreo.run();
+    
+    String name = "";
+    while(GetSearchResultChoreo.available()) {
+      name = GetSearchResultChoreo.readStringUntil('\x1F');
+      name.trim();
+    }
+    int length = name.indexOf('\n');
+    String joke = name.substring(0,length);
+    Serial.println(name);
+    Serial.println(joke);
+    
+    GetSearchResultChoreo.close();
+    
+    if(joke){
+      StatusesUpdateChoreo.addInput("StatusUpdate", joke);
+
+      // Run the Choreo; when results are available, print them to serial
+      StatusesUpdateChoreo.run();
+    
+      while(StatusesUpdateChoreo.available()) {
+        char c = StatusesUpdateChoreo.read();
+        Serial.print(c);
+      }
+      
+      
+    }
+    StatusesUpdateChoreo.close();
   }
- 
+
+  Serial.println("\nWaiting...\n");
+  delay(30000); // wait 30 seconds between GetSearchResult calls
 }
-
-void loop()
-{
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true);
-  }
-}
-
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  //Serial.println("1");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  //Serial.println("2");
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  //Serial.println("3");
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  //Serial.println("4");
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  //Serial.println("5");
-  Udp.endPacket();
-  //Serial.println("6");
-}
-
-
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-}
-
-
-
-
-
-
-
-
-
-
